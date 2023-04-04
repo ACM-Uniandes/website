4b8afa5d5bca5920ac2de744fec88765918b3885
ajaramilloc <a.jaramilloc2@uniandes.edu.co>
2023-04-03 22:03:00 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,24 +208,6 @@
   });
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
@@ -236,10 +218,5 @@
       mirror: false
     })
   });
-
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
 
 })()
